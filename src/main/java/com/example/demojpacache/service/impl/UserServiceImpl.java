37201fa165f646bea6ab559c9d8f9e61df6be96e
Loi <loiphoto19111999@gmail.com>
2023-06-01 10:29:10 +0700

    lock redission
    Signed-off-by: Loi <loiphoto19111999@gmail.com>

--- src/main/java/com/example/demojpacache/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demojpacache/service/impl/UserServiceImpl.java
@@ -49,6 +49,7 @@ public class UserServiceImpl implements UserService {
 //        simulateSlowService();
         return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User Not found"));
     }
+//  Test
 
     @CacheEvict(value = "user", allEntries = true)
     @Override
